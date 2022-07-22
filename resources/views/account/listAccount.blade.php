cd9ff9329c8ad4d552b51f51ce32fc430a915058
fahmiau <fauliyaay@gmail.com>
2022-07-23 05:12:05 +0700

    add action, delete not worked yet

--- resources/views/account/listAccount.blade.php
+++ resources/views/account/listAccount.blade.php
@@ -11,32 +11,34 @@
     </a>
     <div class="ml-8 ">
 
-      <table class="border-collapse border bg-white w-3/4">
+      <table class="border-collapse border bg-white w-10/12">
         <thead>
           <tr class="bg-blue-100">
-            <th class="px-4 py-1 border  w-1/12">No</th>
-            <th class="px-4 py-1 border  w-1/4">Nama Ibu</th>
-            <th class="px-4 py-1 border  w-1/4">Nama Anak</th>
-            <th class="px-4 py-1 border  w-1/2">Alamat</th>
-            <th class="px-4 py-1 border  w-1/2">Action</th>
+            <th class="px-4 py-1 border  w-12">No</th>
+            <th class="px-4 py-1 border  w-3/12">Nama Ibu</th>
+            <th class="px-4 py-1 border  w-3/12">Nama Anak</th>
+            <th class="px-4 py-1 border  w-3/12">Alamat</th>
+            <th class="px-4 py-1 border  w-2/12">Action</th>
             
           </tr>
         </thead>
         <tbody>
           @foreach ($mothers as $mother)
-              
-          <tr class="hover:bg-blue-50">
-            <td class="border px-4 py-1">{{ $loop->iteration }}</td>
-            <td class="border px-4 py-1">{{ $mother->nama }}</td>
-            <td class="border px-4 py-1">
-              @foreach ($mother->childrens as $children)
-                <div>
-                  <a class="underline hover:no-underline" href="{{ url('/detail-anak/'.$children->id) }}">{{ $children->nama}}</a>
-                </div>
-              @endforeach
-            <td class="border px-4 py-1">{{ $mother->alamat }}</td>
-            <td class="border px-4 py-1 text-red-600"><a href="">Edit</a></td>
-          </tr>
+            <tr class="hover:bg-blue-50">
+              <td class="border px-4 py-1">{{ $loop->iteration }}</td>
+              <td class="border px-4 py-1">{{ $mother->nama }}</td>
+              <td class="border px-4 py-1">
+                @foreach ($mother->childrens as $children)
+                  <div>
+                    <a class="underline hover:no-underline" href="{{ url('/detail-anak/'.$children->id) }}">{{ $children->nama}}</a>
+                  </div>
+                @endforeach
+              <td class="border px-4 py-1">{{ $mother->alamat }}</td>
+              <td class="border px-4 py-1 w-32">
+                <a class="block underline hover:no-underline text-blue-600 hover:text-black" href="{{ url('/account/mother/'.$mother->id) }}"><i class="fas fa-edit"></i>EDIT</a>
+                <a class="block underline hover:no-underline text-red-600 hover:text-black" href=""><i class="fas fa-trash-alt"></i>DELETE</a>
+              </td>
+            </tr>
           @endforeach
         </tbody>
       </table>
