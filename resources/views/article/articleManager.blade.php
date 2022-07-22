26b512c34e82ea335a62dbb244cd74301da1d42a
fahmiau <fauliyaay@gmail.com>
2022-07-23 05:09:16 +0700

    add action btn

--- resources/views/article/articleManager.blade.php
+++ resources/views/article/articleManager.blade.php
@@ -11,19 +11,19 @@
   </a>
   {{-- <div class="w-2/3 ml-8 bg-white filter drop-shadow-xl p-8 rounded-xl"> --}}
     
-    <div class="ml-8 ">
+    <div class="ml-8">
 
-      <table class="border-collapse border bg-white w-3/4">
+      <table class="border-collapse border bg-white w-10/12">
         <thead>
           <tr class="bg-blue-100">
-            <th class="px-4 py-1 border  w-1/12">No</th>
-            <th class="px-4 py-1 border  w-1/4">Judul</th>
-            <th class="px-4 py-1 border  w-1/4">Tanggal Publish</th>
-            <th class="px-4 py-1 border  w-1/2">Status</th>
-            <th class="px-4 py-1 border  w-1/2">Penulis</th>
-            <th class="px-4 py-1 border  w-1/2">Views</th>
-            <th class="px-4 py-1 border  w-1/2">Likes</th>
-            <th class="px-4 py-1 border  w-1/2">Action</th>
+            <th class="px-4 py-1 border w-12">No</th>
+            <th class="px-4 py-1 border w-1/4">Judul</th>
+            <th class="px-4 py-1 border w-52">Tanggal Publish</th>
+            <th class="px-4 py-1 border w-1/6">Status</th>
+            <th class="px-4 py-1 border w-1/6">Penulis</th>
+            <th class="px-4 py-1 border w-16">Views</th>
+            <th class="px-4 py-1 border w-16">Likes</th>
+            <th class="px-4 py-1 border w-32">Action</th>
             
           </tr>
         </thead>
@@ -37,7 +37,10 @@
               <td class="border px-4 py-1">{{ $article->author }}</td>
               <td class="border px-4 py-1">{{ $article->views->views }}</td>
               <td class="border px-4 py-1">{{ $article->likes->likes }}</td>
-              <td class="border px-4 py-1"><a class="underline hover:no-underline" href="{{ url('/article/edit/'.$article->slug) }}">EDIT</a></td>
+              <td class="border px-4 py-1">
+                <a class="block underline hover:no-underline text-blue-600 hover:text-black" href="{{ url('/article/edit/'.$article->slug) }}"><i class="fas fa-edit"></i>EDIT</a>
+                <a class="block underline hover:no-underline text-red-600 hover:text-black" href="{{ url('/article/delete/'.$article->slug) }}"><i class="fas fa-trash-alt"></i>DELETE</a>
+              </td>
             </tr>
           @endforeach
         </tbody>
